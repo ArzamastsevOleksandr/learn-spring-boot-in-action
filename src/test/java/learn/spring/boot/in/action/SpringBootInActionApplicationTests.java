fdf2c6caac0f1f6d58eb269f4a2296a801464d7b
Oleksandr Arzamastsev <oleksandr.arzamastsev@softcannery.com>
2021-01-09 21:06:14 +0200

    initial commit

---
+++ src/test/java/learn/spring/boot/in/action/SpringBootInActionApplicationTests.java
@@ -0,0 +1,13 @@
+package learn.spring.boot.in.action;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootInActionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
