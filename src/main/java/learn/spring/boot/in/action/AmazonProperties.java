9c1d31d32574e9cabe162461a17b8ad80688edc9
Oleksandr Arzamastsev <oleksandr.arzamastsev@softcannery.com>
2021-01-14 01:39:00 +0200

    ch03: extract related properties into a single component

---
+++ src/main/java/learn/spring/boot/in/action/AmazonProperties.java
@@ -0,0 +1,17 @@
+package learn.spring.boot.in.action;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@Getter
+@Setter
+@Component
+@ConfigurationProperties(prefix = "amazon")
+public class AmazonProperties {
+
+    private String associateId;
+    private String productUrl;
+
+}
