91392667e2d525347505ebab0df1c2d721be16c6
Oleksandr Arzamastsev <oleksandr.arzamastsev@softcannery.com>
2021-01-14 01:39:00 +0200

    ch03: extract related properties into a single component

--- src/main/java/learn/spring/boot/in/action/controller/BookController.java
+++ src/main/java/learn/spring/boot/in/action/controller/BookController.java
@@ -1,11 +1,10 @@
 package learn.spring.boot.in.action.controller;
 
+import learn.spring.boot.in.action.AmazonProperties;
 import learn.spring.boot.in.action.entity.BookEntity;
 import learn.spring.boot.in.action.repository.BookEntityRepository;
 import lombok.RequiredArgsConstructor;
-import lombok.Setter;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -17,14 +16,10 @@ import java.util.List;
 @Slf4j
 @Controller
 @RequiredArgsConstructor
-@ConfigurationProperties(prefix = "amazon")
 public class BookController {
 
-    @Setter
-    private String associateId;
-    @Setter
-    private String productUrl;
     private final BookEntityRepository bookEntityRepository;
+    private final AmazonProperties amazonProperties;
 
     @GetMapping("/{reader}")
     public String booksByReader(@PathVariable("reader") String reader, Model model) {
@@ -32,8 +27,8 @@ public class BookController {
         if (readingList != null) {
             model.addAttribute("books", readingList);
             model.addAttribute("reader", reader);
-            model.addAttribute("amazonId", associateId);
-            model.addAttribute("productUrl", productUrl);
+            model.addAttribute("amazonId", amazonProperties.getAssociateId());
+            model.addAttribute("productUrl", amazonProperties.getProductUrl());
         }
         return "readingList";
     }
