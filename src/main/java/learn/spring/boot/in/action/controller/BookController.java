c91fc09107f22342b556108b1f46999856d4a6dd
Oleksandr Arzamastsev <oleksandr.arzamastsev@softcannery.com>
2021-01-14 01:35:12 +0200

    ch03: play with properties

--- src/main/java/learn/spring/boot/in/action/controller/BookController.java
+++ src/main/java/learn/spring/boot/in/action/controller/BookController.java
@@ -3,6 +3,9 @@ package learn.spring.boot.in.action.controller;
 import learn.spring.boot.in.action.entity.BookEntity;
 import learn.spring.boot.in.action.repository.BookEntityRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,10 +14,16 @@ import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.List;
 
+@Slf4j
 @Controller
 @RequiredArgsConstructor
+@ConfigurationProperties(prefix = "amazon")
 public class BookController {
 
+    @Setter
+    private String associateId;
+    @Setter
+    private String productUrl;
     private final BookEntityRepository bookEntityRepository;
 
     @GetMapping("/{reader}")
@@ -22,6 +31,9 @@ public class BookController {
         List<BookEntity> readingList = bookEntityRepository.findByReader(reader);
         if (readingList != null) {
             model.addAttribute("books", readingList);
+            model.addAttribute("reader", reader);
+            model.addAttribute("amazonId", associateId);
+            model.addAttribute("productUrl", productUrl);
         }
         return "readingList";
     }
