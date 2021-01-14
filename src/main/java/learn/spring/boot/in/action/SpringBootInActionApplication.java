e70ecbc627d84d6822b6f02b112c080b4c2c8681
Oleksandr Arzamastsev <oleksandr.arzamastsev@softcannery.com>
2021-01-14 03:12:58 +0200

    ch03: configure logging

--- src/main/java/learn/spring/boot/in/action/SpringBootInActionApplication.java
+++ src/main/java/learn/spring/boot/in/action/SpringBootInActionApplication.java
@@ -1,13 +1,36 @@
 package learn.spring.boot.in.action;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
+@Slf4j
 @SpringBootApplication
 public class SpringBootInActionApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringBootInActionApplication.class, args);
+    }
+
+    @PostConstruct
+    public void postConstruct() {
+        log.debug("@PostConstruct");
+        log.info("@PostConstruct");
+        log.error("@PostConstruct");
+        log.warn("@PostConstruct");
+        log.trace("@PostConstruct");
+    }
+
+    @PreDestroy
+    public void preDestroy() {
+        log.debug("@PostConstruct");
+        log.info("@PostConstruct");
+        log.warn("@PostConstruct");
+        log.trace("@PostConstruct");
+        log.error("@PostConstruct");
     }
 
 }
