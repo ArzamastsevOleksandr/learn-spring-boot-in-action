227fc930851044f14b2d43df7dc6001dc28463ff
Oleksandr Arzamastsev <oleksandr.arzamastsev@softcannery.com>
2021-01-15 01:39:29 +0200

    ch04: write a test against the running server on a random port

--- src/test/java/learn/spring/boot/in/action/IntegrationTestWithFullContextAndServer.java
+++ src/test/java/learn/spring/boot/in/action/IntegrationTestWithFullContextAndServer.java
@@ -7,19 +7,26 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.ApplicationContext;
+import org.springframework.http.HttpStatus;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.client.RestTemplate;
 
 import java.util.Arrays;
 import java.util.UUID;
 
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatNoException;
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
-// MinimalIntegrationTest time taken:         367912781
-// IntegrationTestWithFullContext time taken: 2977723422
+// MinimalIntegrationTestWithNoServer      time taken: 367475291
+// IntegrationTestWithFullContextAndServer time taken: 3344633719
 @ActiveProfiles("test")
-@SpringBootTest
-class IntegrationTestWithFullContext {
+@SpringBootTest(
+        webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT,
+        properties = { // overrides the .yml properties
+                "app.prop_01=cc"
+        })
+class IntegrationTestWithFullContextAndServer {
 
     static long start;
 
@@ -34,6 +41,10 @@ class IntegrationTestWithFullContext {
     @Value("${app.prop_07}")
     String prop_07;
 
+    // SB will set the local.server.port for a randomly generated port
+    @Value("${local.server.port}")
+    String port;
+
     @BeforeAll
     static void beforeAll() {
         start = System.nanoTime();
@@ -41,10 +52,11 @@ class IntegrationTestWithFullContext {
 
     @Test
     void contextLoads() {
-        assertThat(prop_01).isEqualTo("aaaaa");
+        assertThat(prop_01).isEqualTo("cc");
         assertThat(prop_02).isEqualTo("bbbbb");
-
         assertThatNoException().isThrownBy(() -> UUID.fromString(prop_07));
+
+        assertTrue(applicationContext.getEnvironment().containsProperty("server.port"));
 
         assertThat(applicationContext.getBean("amazonProperties")).isNotNull();
         assertThat(applicationContext.getBean("testProfileBean")).isNotNull();
@@ -52,11 +64,20 @@ class IntegrationTestWithFullContext {
 
         Arrays.stream(applicationContext.getBeanDefinitionNames())
                 .forEach(System.err::println);
+
+    }
+
+    @Test
+    void pageNotFound() {
+        var restTemplate = new RestTemplate();
+        assertThatThrownBy(() -> restTemplate.getForObject("http://localhost:{port}/no/way", String.class, port))
+                .isInstanceOf(HttpClientErrorException.class)
+                .hasFieldOrPropertyWithValue("statusCode", HttpStatus.NOT_FOUND);
     }
 
     @AfterAll
     static void afterAll() {
-        System.err.println(MinimalIntegrationTest.class.getName() + " taken to execute (ns): " + (System.nanoTime() - start));
+        System.err.println(MinimalIntegrationTestWithNoServer.class.getName() + " taken to execute (ns): " + (System.nanoTime() - start));
     }
 
 }
